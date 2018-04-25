4192d24daff24124c96f8b31cfc8432c0fcbf332
Jacob Yent <35236399+jacobyent@users.noreply.github.com>
2018-04-24 21:37:22 -0700

    Getting started

---
+++ board.cpp
@@ -0,0 +1,5 @@
+#include "board.h"
+#include <iostream>
+
+using namespace std;
+
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//This is the main file for the program. It will read in the board, solve it if it can, and output to the user once finished.
+//
+
+#include <iostream>
+#include "board.h"
+
+using namespace std;
+
+int main()
+{
+	Board my_board;
+	my_board.say_hello();
+	return 0;
+
+}
